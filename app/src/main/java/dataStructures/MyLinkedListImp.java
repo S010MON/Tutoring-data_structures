476557aa6970c77d2912afe269296815ae5724af
S010MON <leon@bdebnath.com>
2021-10-29 09:58:53 +0200

    Implements LinkedList Test

---
+++ app/src/main/java/dataStructures/MyLinkedListImp.java
@@ -0,0 +1,35 @@
+package dataStructures;
+
+public class MyLinkedListImp<E> implements MyLinkedList<E>
+{
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void remove(E element) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
--- app/src/test/java/dataStructures/MyLinkedListTest.java
+++ app/src/test/java/dataStructures/MyLinkedListTest.java
@@ -8,26 +8,51 @@ public class MyLinkedListTest
 {
     @Test void testAdd()
     {
-
+        MyLinkedList<Integer> list = new MyLinkedListImp<>();
+        for(int i = 0; i < 1000; i++)
+        {
+            list.add(i);
+        }
     }
 
     @Test void testGet()
     {
-
+        MyLinkedList<Integer> list = new MyLinkedListImp<>();
+        for(int i = 0; i < 1000; i++)
+        {
+            list.add(i);
+        }
+        assertEquals(0, list.get(0));
+        assertEquals(10, list.get(10));
+        assertEquals(999, list.get(999));
     }
 
     @Test void testRemove()
     {
-
+        MyLinkedList<Integer> list = new MyLinkedListImp<>();
+        for(int i = 0; i < 1000; i++)
+        {
+            list.add(i);
+        }
+        assertEquals(0, list.get(0));
+        list.remove(0);
+        assertEquals(1, list.get(0));
+        assertEquals(999, list.get(998));
     }
 
     @Test void testSize_0()
     {
-
+        MyLinkedList<Integer> list = new MyLinkedListImp<>();
+        assertEquals(0, list.size());
     }
 
     @Test void testSize_gt0()
     {
-
+        MyLinkedList<Integer> list = new MyLinkedListImp<>();
+        for(int i = 0; i < 1000; i++)
+        {
+            list.add(i);
+        }
+        assertEquals(1000, list.size());
     }
 }
