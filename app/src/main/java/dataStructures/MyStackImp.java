bebfe4dfb3eb82926cd935a5ddec60573f19a216
S010MON <leon@bdebnath.com>
2021-10-29 09:59:08 +0200

    Implements MyStack

---
+++ app/src/main/java/dataStructures/MyStackImp.java
@@ -0,0 +1,25 @@
+package dataStructures;
+
+public class MyStackImp<E> implements MyStack<E>
+{
+
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
