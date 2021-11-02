6426424dcc705e011c21463d8139bbbe7df74ead
S010MON <leon@bdebnath.com>
2021-11-02 09:22:08 +0100

    Comments added

--- app/src/main/java/dataStructures/MyQueueImp.java
+++ app/src/main/java/dataStructures/MyQueueImp.java
@@ -8,26 +8,49 @@ public class MyQueueImp<T> implements MyQueue<T>
     private Stack<T> stack1;
     private Stack<T> stack2;
 
+    /**
+     * A two stack implementation of a First In Last Out queue
+     */
     public MyQueueImp()
     {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
     }
 
+    /**
+     * Adds a new element to the back of the queue
+     * @param element
+     */
     @Override
     public void enqueue(T element)
     {
 
     }
 
+    /**
+     * Removes an element from the front of the queue
+     * @return
+     */
     @Override
     public T dequeue()
     {
         return null;
     }
 
+    /**
+     * Using two stacks, figure out when each stack must be used
+     */
+    private void transferStacks()
+    {
+
+    }
+
+    /**
+     * @return true if both stacks are empty
+     */
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return false;
     }
 }
