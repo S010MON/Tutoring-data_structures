a6cf7a9b49fbcd481da8889d4a6d950bcb3512d8
S010MON <leon@bdebnath.com>
2021-11-02 09:05:06 +0100

    Queue test created

---
+++ app/src/test/java/dataStructures/MyQueueTest.java
@@ -0,0 +1,55 @@
+package dataStructures;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class MyQueueTest
+{
+    @Test void testEnqueue()
+    {
+        MyQueue<Integer> Q = new MyQueueExample<>();
+        for (int i = 0; i < 1000; i++)
+        {
+            Q.enqueue(i);
+        }
+    }
+
+    @Test void testDequeue()
+    {
+        MyQueue<Integer> Q = new MyQueueExample<>();
+        for (int i = 0; i < 1000; i++)
+        {
+            Q.enqueue(i);
+        }
+        for (int i = 0; i < 1000; i++)
+        {
+            assertEquals(i, Q.dequeue());
+        }
+    }
+
+    @Test void isEmpty_true_start()
+    {
+        MyQueue<Integer> Q = new MyQueueExample<>();
+        assertTrue(Q.isEmpty());
+    }
+
+    @Test void isEmpty_true_after()
+    {
+        MyQueue<Integer> Q = new MyQueueExample<>();
+        Q.enqueue(1);
+        Q.dequeue();
+        assertTrue(Q.isEmpty());
+    }
+
+    @Test void isEmpty_false()
+    {
+        MyQueue<Integer> Q = new MyQueueExample<>();
+        Q.enqueue(1);
+        assertFalse(Q.isEmpty());
+    }
+}
