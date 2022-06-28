6429fbac335a939a8a556ec8d7270077974b57b1
S010MON <leon_debnath@hotmail.co.uk>
2022-06-28 11:12:05 +0200

    add implements `MyHashMap`

--- app/src/main/java/dataStructures/hashMap/MyHashMapImp.java
+++ app/src/main/java/dataStructures/hashMap/MyHashMapImp.java
@@ -1,6 +1,29 @@
 package dataStructures.hashMap;
 
-public class MyHashMapImp
+public class MyHashMapImp<K, V> implements MyHashMap<K, V>
 {
 
+    @Override
+    public void put(K key, V value)
+    {
+
+    }
+
+    @Override
+    public V get(K key)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return false;
+    }
 }
