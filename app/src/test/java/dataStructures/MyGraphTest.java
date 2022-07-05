f703c43dcd30205af9e466917351e683b768288c
S010MON <leon@bdebnath.com>
2022-07-05 14:32:54 +0100

    adds Edge tests

--- app/src/test/java/dataStructures/MyGraphTest.java
+++ app/src/test/java/dataStructures/MyGraphTest.java
@@ -26,4 +26,45 @@ public class MyGraphTest
 
         assertEquals(1, G.vertices());
     }
+
+    @Test void testAddEdge()
+    {
+        MyGraph<Integer> G = new MyGraphEdgeListExample<>();
+
+        G.addVertex(1);
+        G.addVertex(2);
+        G.addEdge(1,2);
+
+        assertEquals(1, G.edges());
+    }
+
+    @Test void testAddEdgeMany()
+    {
+        MyGraph<Integer> G = new MyGraphEdgeListExample<>();
+
+        for(int i = 0; i < 100 ; i++)
+            G.addVertex(i);
+
+        for(int i = 1; i < 100 ; i++)
+            G.addEdge(0, i);
+
+        assertEquals(99, G.edges());
+    }
+
+    @Test void testAddEdgeRepeated()
+    {
+        MyGraph<Integer> G = new MyGraphEdgeListExample<>();
+
+        G.addVertex(1);
+        G.addVertex(2);
+        G.addEdge(1,2);
+        G.addEdge(1,2);
+
+        if(G.edges() == 2)
+        {
+            System.out.println("\n**NOTE** Two of the same edge were added - when adding an edge check whether it is already in graph!");
+        }
+
+        assertEquals(1, G.edges());
+    }
 }
